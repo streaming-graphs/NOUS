e880e0beb1135a1281e203e927ea6f6d6d5effb2
Sumit Purohit <purohitsumit@gmail.com>
2016-08-25 03:42:54 -0700

    bug fixed and testing done to generate GIP

--- knowledge_graph/src/main/scala/gov/pnnl/aristotle/algorithms/GraphMinerV4.scala
+++ knowledge_graph/src/main/scala/gov/pnnl/aristotle/algorithms/GraphMinerV4.scala
@@ -13,37 +13,38 @@ import scala.io.Source
 import gov.pnnl.aristotle.algorithms.mining.v3.CandidateGenerationV4
 import java.io.File
 
+
 /**
  * @author puro755
  *
  */
 object GraphMinerV4 {
 
-   /*
+  /*
    * Initialize the spark context and its run-time configurations.
    * Remove .setMaster("local") before running this on a cluster
    */
 
- val sc = SparkContextInitializer.sc
+  val sc = SparkContextInitializer.sc
 
   /*
    * Initialize Log File Writer & Set up the Logging level
    */
-  val writerSG = new PrintWriter( new File( "GraphMiningOutputV4.txt" ) )
-  Logger.getLogger( "org" ).setLevel( Level.DEBUG )
-  Logger.getLogger( "akka" ).setLevel( Level.DEBUG )
-  
+  val writerSG = new PrintWriter(new File("GraphMiningOutputV4.txt"))
+  Logger.getLogger("org").setLevel(Level.OFF)
+  Logger.getLogger("akka").setLevel(Level.OFF)
+
   def main(args: Array[String]): Unit = {
-   
-   var t_sc0 = System.nanoTime();
-   
-   if(args.length != 5) {
-      println("Usage: <edge label denoting node type(Int)> <minSupport> " + 
-          " <type threshold for node> <numIterations> "  + 
-          " <BatchInfoFile (containingPathToDataDirectoriesPerBatch)>")
+
+    var t_sc0 = System.nanoTime();
+
+    if (args.length != 5) {
+      println("Usage: <edge label denoting node type(Int)> <minSupport> " +
+        " <type threshold for node> <numIterations> " +
+        " <BatchInfoFile (containingPathToDataDirectoriesPerBatch)>")
       exit
     }
-   
+
     /*
 	 * Read multiple files. Each file is treated as a new batch.
 	 */
@@ -53,11 +54,11 @@ object GraphMinerV4 {
     val numIter = args(3).toInt
     val listBatchFiles = args(4)
     val windowSize = 5
-//    val window = new WindowStateV3()
-//    val window_metrics = new WindowMetrics()
-   
+    //    val window = new WindowStateV3()
+    //    val window_metrics = new WindowMetrics()
+
     var batch_id = -1;
-    var gWin = new CandidateGenerationV4( minSup )
+    var gWin = new CandidateGenerationV4(minSup)
     //var pattern_trend : Map[String, List[( Int, Int )]] = Map.empty
 
     /*
@@ -67,7 +68,6 @@ object GraphMinerV4 {
       filepath <- Source.fromFile(listBatchFiles).
         getLines().filter(str => !str.startsWith("#"))
     ) {
-
      /*
      * batch_id: each files is read as a new batch.
      */
@@ -75,12 +75,23 @@ object GraphMinerV4 {
       println(s"******Reading File $filepath with batch_id= $batch_id")
       //val batch_metrics = new BatchMetrics( batch_id )
 
-      val input_graph = ReadHugeGraph.getGraphFileTypeInt( filepath, sc )
-      
+      val input_graph = ReadHugeGraph.getGraphFileTypeInt(filepath, sc)
+
       t_sc0 = System.nanoTime()
-      val gBatch = new CandidateGenerationV4(minSup).init(sc, input_graph, writerSG, baseEdgeType, nodeTypeThreshold)
+      val GIP = new CandidateGenerationV4(minSup).init(sc, input_graph, writerSG, baseEdgeType, nodeTypeThreshold)
+
+      val gip_vertices_4degree = GIP.degrees.filter(v => v._2 == 4).count
+      val gip_vertices_1degree = GIP.degrees.filter(v => v._2 == 1).count
+      val gip_vertices_2degree = GIP.degrees.filter(v => v._2 == 2).count
+      val gip_vertices_3degree = GIP.degrees.filter(v => v._2 == 3).count
+
+      println("total GIP nodes", GIP.vertices.count)
+      println("nodes with 1 degree = ", gip_vertices_1degree)
+      println("nodes with 2 degree = ", gip_vertices_2degree)
+      println("nodes with 3 degree = ", gip_vertices_3degree)
+      println("nodes with 4 degree = ", gip_vertices_4degree)
     }
-    
- }
+
+  }
 
 }
--- knowledge_graph/src/main/scala/gov/pnnl/aristotle/algorithms/mining/v3/CandidateGenerationV4.scala
+++ knowledge_graph/src/main/scala/gov/pnnl/aristotle/algorithms/mining/v3/CandidateGenerationV4.scala
@@ -20,7 +20,7 @@ import org.apache.spark.graphx.Edge
 import gov.pnnl.aristotle.algorithms.mining.GraphProfiling
 import gov.pnnl.aristotle.algorithms.mining.datamodel.PatternInstance
 import gov.pnnl.aristotle.algorithms.mining.datamodel.PatternInstanceNode
-import gov.pnnl.aristotle.algorithms.mining.datamodel.PatternInstanceNode
+import org.apache.spark.rdd.RDD
 import gov.pnnl.aristotle.algorithms.mining.datamodel.PatternInstanceNode
 
 /**
@@ -38,7 +38,7 @@ class CandidateGenerationV4(val minSup: Int) extends Serializable {
   
   
     def init(sc : SparkContext, graph: Graph[Int, KGEdgeInt], writerSG: PrintWriter, basetype: Int,
-    type_support: Int): CandidateGenerationV4 = {
+    type_support: Int):  Graph[PatternInstanceNode, Int] = {
 
     /*
      * Get all the rdf:type dst node information on the source node
@@ -49,46 +49,7 @@ class CandidateGenerationV4(val minSup: Int) extends Serializable {
 
     // Now we have the type information collected in the original graph
     val typedAugmentedGraph: Graph[(Int, Map[Int, Int]), KGEdgeInt] = getTypedGraph(graph, writerSG)
-    
-    
-    /*
-     * Create GIP from this graph
-     * 
-     * Every node has following structure:
-     * (Long,(List[Int],Set[PatternInstance],Long)
-     * Long: VertexId
-     * List[Int]: pattern key
-     * Set[PatternInstance]: pattern instances around the local edge 
-     * Long : timestamp of that pattern edge
-     */
-    val all_gip_nodes = typedAugmentedGraph.triplets.flatMap(triple=>{
-      
-      //Local Execution on a triple edge; but needs source and destination
-      val source_node = triple.srcAttr
-      val destination_node = triple.dstAttr
-      val time_stamp = triple.attr.getdatetime
-      
-      val all_src_types = source_node._2.keys
-      val all_dst_types = destination_node._2.keys
-      var all_local_gip_vertices: 
-      scala.collection.mutable.Set[(Long,PatternInstanceNode)] 
-    		  = scala.collection.mutable.Set.empty
-      for(src_type <- all_src_types)
-        for(dst_type <- all_dst_types)
-        {
-          val gip_v_key = (src_type+dst_type).hashCode.toLong
-          val gip_v_label = List(src_type,triple.attr.getlabel)
-          val gip_v_instance = Set(new 
-              PatternInstance(
-                  Set((triple.srcAttr._1,triple.dstAttr._1))))
-          all_local_gip_vertices += ((gip_v_key,new PatternInstanceNode(gip_v_label,gip_v_instance,time_stamp)))
-        }
-      all_local_gip_vertices
-    })
-    
-    
-    
-    
+
     /*
      * Create RDD where Every vertex has all the 1 edge patterns it belongs to
      * Ex: Sumit: (person worksAt organizaion) , (person friendsWith person)
@@ -98,45 +59,13 @@ class CandidateGenerationV4(val minSup: Int) extends Serializable {
      * ....
      * 
      */
-    val nonTypedVertexRDD: VertexRDD[Array[(List[Int], Set[PatternInstance])]] = getOneEdgePatterns(typedAugmentedGraph)    
+    val oneEdgePatternOnVertexRDD: VertexRDD[Array[(List[Int], PatternInstance)]] = getOneEdgePatternsRDD(typedAugmentedGraph)    
 
-    //val nonTypedVertexRDD : VertexRDD[Array[(List[Int], Long)]] =  getCondensedVRDD(nonTypedVertexRDD_flat)
-
-    /*
-     * Create Vertices of the GIP
-     * GIP is a property graph so every nodes keeps some extra information
-     * Starting with pattern_edge, and data_edge 
-     * NOTE: map vertices can not provide timestamp, must be constructed from edge
-    val gip_vertices = nonTypedVertexRDD.flatMap(vertex=>{
-      vertex._2.map(a_pattern
-          =>((a_pattern._1.toString()+a_pattern._2.toString).hashCode().toLong,(a_pattern._1,a_pattern._2)))
-    })
-    */
-    /*
-     * Create Edges of the GIP
-     */
-    val gip_edge = nonTypedVertexRDD.flatMap(vertex=>{
-      var all_gip_vertices: scala.collection.mutable.Set[Long] = scala.collection.mutable.Set.empty
-      vertex._2.map(a_pattern => all_gip_vertices +=
-        ((a_pattern._1.toString() + a_pattern._2.toString).hashCode().toLong))
-      val all_gip_vertices_list = all_gip_vertices.toList
-      // make a list to get an order becase we need to cross join them
-      // in next step.
-      var i = 0
-      var j = 0
-      var local_edges: scala.collection.mutable.Set[Edge[Int]] = scala.collection.mutable.Set.empty
-      for (i <- 0 to all_gip_vertices_list.length - 1) {
-        for (j <- i to all_gip_vertices_list.length - 1) {
-          if (all_gip_vertices_list(i) != all_gip_vertices_list(j))
-            local_edges += Edge(all_gip_vertices_list(i), all_gip_vertices_list(j), 1)
-        }
-      }
-      local_edges
-    })
+    val gipVertices = getGIPVertices(typedAugmentedGraph)
     
-    val gip =  Graph(all_gip_nodes,gip_edge)
-    writerSG.flush()
-    return this
+    val gipEdge = getGIPEdges(oneEdgePatternOnVertexRDD)
+    
+    return  Graph(gipVertices,gipEdge)
   }
   
     def getTypedGraph(graph: Graph[Int, KGEdgeInt],
@@ -150,27 +79,95 @@ class CandidateGenerationV4(val minSup: Int) extends Serializable {
         writerSG, typedVertexRDD)
       return typedAugmentedGraph
     }
+ 
+ def getGIPEdges(oneEdgePatternOnVertexRDD: VertexRDD[Array[(List[Int], PatternInstance)]]) :
+ RDD[Edge[Int]] =
+ {
+     /*
+     * Create Edges of the GIP
+     * 
+     * getGIPVertices has similar code to find Vertices, but for Vertices, we need to carry much more data.
+     * Also if we try to store fat data on every vertex, it may become bottleneck
+     * 
+     */
+      val gipEdges = oneEdgePatternOnVertexRDD.flatMap(vertex => {
+        var all_gip_vertices: scala.collection.mutable.ListBuffer[List[Int]] = scala.collection.mutable.ListBuffer.empty
+        vertex._2.map(a_pattern => all_gip_vertices +=
+          (a_pattern._1 ++ List(a_pattern._2.get_instacne.head._1, a_pattern._2.get_instacne.head._2)))
+        // 'head' is used here because at this point of time it is only an one-edge instance
+        val all_gip_vertices_list = all_gip_vertices.toList
+        // make a list to get an order because we need to cross join them
+        // in next step.
+        var i = 0 ; var j = 0
+        var local_edges: scala.collection.mutable.Set[Edge[Int]] = scala.collection.mutable.Set.empty
+        for (i <- 0 to all_gip_vertices_list.length - 1) {
+          for (j <- i to all_gip_vertices_list.length - 1) {
+            if (all_gip_vertices_list(i) != all_gip_vertices_list(j))
+              local_edges += Edge(all_gip_vertices_list(i).hashCode, all_gip_vertices_list(j).hashCode, 1)
+          }
+        }
+        local_edges
+      })
+      return gipEdges
+    }
     
-      def getOneEdgePatterns(typedAugmentedGraph: Graph[(Int,  
-    Map[Int, Int]), KGEdgeInt]): VertexRDD[Array[(List[Int],Set[PatternInstance])]] =
+ def getGIPVertices(typedAugmentedGraph: Graph[(Int, Map[Int, Int]), KGEdgeInt] ) :
+ RDD[(Long,PatternInstanceNode)] =
+ {
+       /*
+     * Create GIP from this graph
+     * 
+     * Every node has following structure:
+     * (Long,(List[Int],Set[PatternInstance],Long)
+     * Long: VertexId
+     * List[Int]: pattern key
+     * Set[PatternInstance]: pattern instances around the local edge 
+     * Long : timestamp of that pattern edge
+     */
+    val allGIPNodes : RDD[(Long,PatternInstanceNode)]= 
+      typedAugmentedGraph.triplets.filter(triple=>triple.attr.getlabel != TYPE).flatMap(triple => {
+
+      //Local Execution on a triple edge; but needs source and destination
+      val source_node = triple.srcAttr
+      val destination_node = triple.dstAttr
+      val time_stamp = triple.attr.getdatetime
+      val all_src_types = source_node._2.keys
+      val all_dst_types = destination_node._2.keys
+
+      var all_local_gip_vertices: scala.collection.mutable.Set[(Long, PatternInstanceNode)] = scala.collection.mutable.Set.empty
+      
+      for (src_type <- all_src_types)
+        for (dst_type <- all_dst_types) {
+          val gip_v_key = List(src_type , triple.attr.getlabel, dst_type, triple.srcAttr._1, triple.dstAttr._1).hashCode
+          val gip_v_label = List(src_type , triple.attr.getlabel, dst_type, triple.srcAttr._1, triple.dstAttr._1)
+          val gip_v_instance = new PatternInstance(Set((triple.srcAttr._1, triple.dstAttr._1)))
+          all_local_gip_vertices += ((gip_v_key, new PatternInstanceNode(gip_v_label, gip_v_instance, time_stamp)))
+        }
+      all_local_gip_vertices
+    })
+    return allGIPNodes
+ }
+    
+ def getOneEdgePatternsRDD(typedAugmentedGraph: Graph[(Int,  
+    Map[Int, Int]), KGEdgeInt]): VertexRDD[Array[(List[Int],PatternInstance)]] =
     {
-      return typedAugmentedGraph.aggregateMessages[Array[(List[Int], Set[PatternInstance])]](
+      return typedAugmentedGraph.aggregateMessages[Array[(List[Int], PatternInstance)]](
         edge => {
           if (edge.attr.getlabel != TYPE) {
             // Extra info for pattern
             if ((edge.srcAttr._2.size > 0) &&
               (edge.dstAttr._2.size > 0)) {
-              val dstnodetype = edge.dstAttr._2
-              val srcnodetype = edge.srcAttr._2
+              val dstnodetype = edge.dstAttr._2.keys
+              val srcnodetype = edge.srcAttr._2.keys
               srcnodetype.foreach(s => {
                 dstnodetype.foreach(d => {
-                  var pattern_instance: scala.collection.immutable.Set[(Int, Int)] = Set.empty
-                  edge.sendToSrc(Array(List(s._1, edge.attr.getlabel,
-                    d._1, -1)
-                    -> Set(new PatternInstance(pattern_instance))))
-                  edge.sendToDst(Array(List(s._1, edge.attr.getlabel,
-                    d._1, -1)
-                    -> Set(new PatternInstance(pattern_instance))))
+                  var pattern_instance: scala.collection.immutable.Set[(Int, Int)] = Set((edge.srcAttr._1,edge.dstAttr._1))
+                  edge.sendToSrc(Array(List(s, edge.attr.getlabel,
+                    d)
+                    -> new PatternInstance(pattern_instance)))
+                  edge.sendToDst(Array(List(s, edge.attr.getlabel,
+                    d)
+                    -> new PatternInstance(pattern_instance)))
                 })
               })
             }
@@ -180,8 +177,9 @@ class CandidateGenerationV4(val minSup: Int) extends Serializable {
           reducePatternsOnNodeV2(pattern1NodeN, pattern2NodeN)
         })
     }
-  def reducePatternsOnNodeV2(a: Array[(List[Int], Set[PatternInstance])], b: Array[(List[Int], Set[PatternInstance])]): 
-	  Array[(List[Int], Set[PatternInstance])] =
+  def reducePatternsOnNodeV2(a: Array[(List[Int], PatternInstance)], 
+      b: Array[(List[Int], PatternInstance)]): 
+	  Array[(List[Int], PatternInstance)] =
     {
       
     return a ++ b
@@ -201,5 +199,60 @@ class CandidateGenerationV4(val minSup: Int) extends Serializable {
 		})
 	}
   
+  def computeMinImageSupport(input_gpi : Graph[(Long,PatternInstanceNode), Int])
+	  :RDD[(List[Int],Int)] =
+  {
+
+      /*
+     * A flat RDD like:
+     * (P1,person,sp)
+     * (P1,person,sc)
+     * (P1,org,pnnl)
+     * (P1,org,pnnl)
+     */
+      val sub_pattern_key_rdd = input_gpi.vertices.flatMap(vertext => {
+        vertext._2._2.pattern_instance_map.map(pattern_instance_pair => {
+          ((vertext._2._2.pattern, pattern_instance_pair._1, pattern_instance_pair._2))
+        })
+      }).distinct
+      //.reduceByKey((sub_pattern_instance_count1, sub_pattern_instance_count2) => sub_pattern_instance_count1 + sub_pattern_instance_count2)
+
+      
+      val mis_rdd = sub_pattern_key_rdd.map(key=>{
+        ((key._1, key._2),1)
+         /*
+         * ((P1,person) , 1) from (P1,person,sp)
+         * ((P1,person) , 1) from (P1,person,sc)
+         * ((P1,org) , 1) from (P1,org,pnnl)
+         * 
+         */
+
+      }).reduceByKey((unique_instance1_count,unique_instance2_count) 
+          => unique_instance1_count + unique_instance2_count)
+      
+     /*
+     * Input is 'mis_rdd' which is a Cumulative RDD like:
+     * P1:person, 2
+     * P1:org, 1
+     * 
+     * Output is patternSup which gets minimum of all P1:x 
+     * so return (P1, 1)
+     */
+      val patternSup: RDD[(List[Int], Int)] = mis_rdd.map(sup_pattern_key => {
+        //Emitt (P1, 2) and (P1 1)
+       (sup_pattern_key._1._1,sup_pattern_key._2)
+      }).reduceByKey((full_pattern_instace_count1, full_pattern_instace_count2) => {
+        /*
+       * Not using Math lib to because it loads entire dir for min function.
+       * Also seen it failing in cluster mode.
+       */
+        if (full_pattern_instace_count1 < full_pattern_instace_count2)
+          full_pattern_instace_count1
+        else
+          full_pattern_instace_count2
+      })
+
+      return patternSup
+    }
   
 }
